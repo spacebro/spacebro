d7f14456ace691a1fcc3875e6ee0bab1f92e5eea
hugohil <hugo@soixantecircuits.fr>
2015-12-30 16:16:28 +0100

    Fix last commit

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ io.on('connection', function (socket) {
       console.log('disconnect'.bold.red)
     })
     .on('error', function (err) {
-      console.log('error'.bold.red)
-    // console.log(err)
+      console.log('server.js - socket error: %s'.bold.red, err)
     })
     .on('write-img', function (imgDataURL) {
       let base64Data = imgDataURL.replace(/^data:image\/png;base64,/, '')
@@ -38,20 +37,25 @@ io.on('connection', function (socket) {
       let destinationFile = pathHelper.join(path, name)
       let tmpDestinationFile = pathHelper.join(tmpPath, name)
 
-      console.log('server.js - tmpDestinationFile:', tmpDestinationFile)
-
-      fs.writeFile(tmpDestinationFile, base64Data, 'base64', function (err) {
+      fs.writeFile(tmpDestinationFile, base64Data, 'base64', (err) => {
         if (err) {
-          console.log('server.js - error while writing: '.bold.red)
-          console.log(err)
+          console.log('server.js - error writing tmp: %s'.bold.red, err)
         } else {
-          fs.writeFileSync(destinationFile, imagemagick.convert({
+          console.log('server.js - tmp file %s written'.cyan, tmpDestinationFile)
+          let resized = imagemagick.convert({
             srcData: fs.readFileSync(tmpDestinationFile),
             width: config.image.resized.width,
             height: config.image.resized.height,
             resizeStyle: config.image.resized.style, // is the default, or 'aspectfit' or 'fill'
             gravity: config.image.resized.gravity // optional: position crop area when using 'aspectfill'
-          }))
+          })
+          fs.writeFileSync(destinationFile, resized, {encoding: 'base64'}, (err) => {
+            if (err) {
+              console.log('server.js - error writing file: %s'.bold.red, err)
+            } else {
+              console.log('server.js - file %s written'.green, destinationFile)
+            }
+          })
         }
       })
     })
