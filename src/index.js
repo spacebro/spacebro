461b2771672dfa0d4ccdbf7759fb3ae79e49d43d
brendan <brendan@soixantecircuits.fr>
2016-08-23 17:34:01 +0200

    fix quitting

--- src/index.js
+++ src/index.js
@@ -116,7 +116,7 @@ function joinChannel (socket, channelName) {
 
 function quitChannel (socket, channelName) {
   if (!_.has(infos, channelName)) infos[channelName] = { events: [], clients: [] }
-  infos[channelName].clients = _.remove(infos[channelName].clients, socket.clientName)
+  log(_.remove(infos[channelName].clients, s => s === socket.clientName))
   dashboard.setInfos(infos)
 }
 
