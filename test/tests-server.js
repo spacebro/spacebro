c543f020436aee63efde8533c4d9590d64c7989e
Olivier FAURE <olivier.faure@epitech.eu>
2017-07-26 15:26:53 +0200

    :white_check_mark: Add message tests

--- test/tests-server.js
+++ test/tests-server.js
@@ -64,3 +64,51 @@ test('Message - all', async (t) => {
 
   t.deepEqual(messages, ['world', 'world'])
 })
+
+test('Message - _to', async (t) => {
+  initServer('test-channel-message-_to')
+  const client1 = initClient('test-channel-message-_to', 'client1')
+  const client2 = initClient('test-channel-message-_to', 'client2')
+  const client3 = initClient('test-channel-message-_to', 'client3')
+  port++
+
+  await sleep(waitTime)
+
+  client1.sendTo('hello', 'client2', { str: 'world' })
+
+  const messages = []
+  client1.on('hello', data => t.fail())
+  client2.on('hello', data => messages.push(data.str))
+  client3.on('hello', data => t.fail())
+
+  await sleep(waitTime)
+
+  t.deepEqual(messages, ['world'])
+})
+
+test('Message - connection', async (t) => {
+  initServer('test-channel-message-connection')
+  const client1 = initClient('test-channel-message-connection', 'client1')
+  const client2 = initClient('test-channel-message-connection', 'client2')
+  const client3 = initClient('test-channel-message-connection', 'client3')
+  port++
+
+  await sleep(waitTime)
+
+  client1.emit('helloOut', 'world')
+  getGraph('test-channel-message-connection').addConnections([
+    {
+      src: { clientName: 'client1', eventName: 'helloOut' },
+      tgt: { clientName: 'client3', eventName: 'helloIn' }
+    }
+  ])
+
+  const messages = []
+  client1.on('*', data => t.not(data, 'world'))
+  client2.on('*', data => t.not(data, 'world'))
+  client3.on('helloIn', data => messages.push(data))
+
+  await sleep(waitTime)
+
+  t.deepEqual(messages, ['world'])
+})
