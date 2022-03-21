6bd731b980f60ec5452a7e255c5bf02aa69e3844
gabrielstuff <gabriel@soixantecircuits.fr>
2022-03-21 01:01:17 +0100

    🐞🧹 - add eslint and update gulp

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,15 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 7
-  },
-  // https://github.com/feross/standard/blob/master/RULES.md
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'promise'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  },
   env: {
-    mocha: true,
-    es6: true,
+    commonjs: true,
+    es2021: true,
     node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
   }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,33 @@ const nodemon = require('gulp-nodemon')
 const babel = require('gulp-babel')
 const Cache = require('gulp-file-cache')
 
-gulp.task('default', ['watch', 'compile'])
-
-var cache = new Cache();
+const cache = new Cache()
 
 gulp.task('compile', function () {
-  var stream = gulp.src('./src/**/*.js') // your ES2015 code
-                   .pipe(cache.filter()) // remember files
-                   .pipe(babel({ presets: ['es2015'], plugins: ['transform-runtime'] })) // compile new ones
-                   .pipe(cache.cache()) // cache them
-                   .pipe(gulp.dest('./dist')) // write them
+  var stream = gulp
+    .src('./src/**/*.js') // your ES2015 code
+    .pipe(cache.filter()) // remember files
+    .pipe(babel({ presets: ['es2015'], plugins: ['transform-runtime'] })) // compile new ones
+    .pipe(cache.cache()) // cache them
+    .pipe(gulp.dest('./dist')) // write them
   return stream // important for gulp-nodemon to wait for completion
 })
 
-gulp.task('watch', ['compile'], function () {
+gulp.task('watch', function (done) {
   var stream = nodemon({
-                 script: 'index.js' // run ES5 code
-               , watch: 'src' // watch ES2015 code
-               , tasks: ['compile'] // compile synchronously onChange
-               })
-
-  return stream
+    script: 'index.js', // run ES5 code
+    watch: 'src', // watch ES2015 code
+    tasks: ['compile'],
+    done: done // compile synchronously onChange
+  })
+  stream
+    .on('restart', function () {
+      console.log('restarted!')
+    })
+    .on('crash', function () {
+      console.error('Application has crashed!\n')
+      stream.emit('restart', 10) // restart the server in 10 seconds
+    })
 })
+
+gulp.task('default', gulp.series('watch', 'compile'))
